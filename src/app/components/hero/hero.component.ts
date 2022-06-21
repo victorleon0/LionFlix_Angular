0b42b40fb56574e86a747fe158faab95f9e4ef3a
victorleon0 <victor.leon.huerta@gmail.com>
2022-06-21 18:27:31 +0100

    FEAT: Add Hero with styles

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -1,3 +1,4 @@
+import { HeroInterface } from './../../models/films.interfaces';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroComponent implements OnInit {
 
-  constructor() { }
+  public principalHome : HeroInterface
+
+  constructor() { 
+    this.principalHome = {
+      title: 'JURASSIC WORLD - Dominion',
+      gallery: {
+        src: '../../assets/images/Video/JURASSIC.mp4',
+        alt: "Trailer"
+      },
+      play: {
+        src: 'https://pixsector.com/cache/0d0aeff3/av63fa1d6082bbbeb54d8.png',
+        text: "Play"
+      },
+      more: {
+        src: 'https://icon-library.com/images/information-icon-white/information-icon-white-3.jpg',
+        text: "More Info"
+      }
+    }
+  }
 
   ngOnInit(): void {
   }
