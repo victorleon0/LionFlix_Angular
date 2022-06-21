08d0cbd1630ef15960746d025064f49c77d2cb6e
victorleon0 <victor.leon.huerta@gmail.com>
2022-06-21 19:33:38 +0100

    Fix: Styles and animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,33 +11,89 @@ export class AppComponent {
   public horrormovies: MoviesInterface = {
     title: "Horror Movies",
     gallery: [{
-      src:"https://upload.wikimedia.org/wikipedia/ca/3/3e/Crash_Bandicoot_3_Warped_Original_Box_Art.jpg",
-      alt: "CB3"
+      src:"https://cdn.mos.cms.futurecdn.net/o3Huwi7H8UFE4tQLXXRLqc.jpg",
+      alt: "Scream"
     },
     {
-      src:"https://m.media-amazon.com/images/I/51KXBNSFJ1L._AC_SX466_.jpg",
-      alt: "Tenchu"
+      src:"https://media.diariolasamericas.com/p/b5603635b535cac87b238874a2c4328b/adjuntos/216/imagenes/002/305/0002305389/a-quiet-place-part-ap.jpg",
+      alt: "Quiet Place"
     },
     {
-      src:"https://m.media-amazon.com/images/I/61KPFN43GSL.jpg",
-      alt: "Vagrant"
+      src:"https://images2.minutemediacdn.com/image/fetch/c_fill,g_auto,f_auto,h_2080,w_3200/https%3A%2F%2Fnetflixlife.com%2Fwp-content%2Fuploads%2Fgetty-images%2F2022%2F03%2F1347832564.jpeg",
+      alt: "Exhorcism"
     },
     {
-      src:"https://static.wikia.nocookie.net/esfinalfantasy/images/4/40/Portada_FFVII_PSX_E.jpg",
-      alt: "FFVII"
+      src:"https://creepycatalog.com/wp-content/uploads/sites/2/2022/02/student_body_2022_killer_in_the_hallway.jpg?w=768",
+      alt: "Creepy"
     },
     {
-      src:"https://as01.epimg.net/meristation/imagenes/2015/04/30/reportaje/1430373600_448371_1532453664_sumario_normal.png",
-      alt: "Silent"
+      src:"https://www.slashfilm.com/img/gallery/the-horror-movies-we-cant-wait-to-see-in-2022/scream-1641937103.jpg",
+      alt: "Knife"
     },
     {
-      src:"https://upload.wikimedia.org/wikipedia/en/a/ab/Abe%27s_Oddysee_Cover.jpg",
-      alt: "Abe"
+      src:"https://i.ytimg.com/vi/3eGP6im8AZA/maxresdefault.jpg",
+      alt: "Black Phone"
+    }]
+   }
+
+
+   public popularmovies: MoviesInterface = {
+    title: "Popular On Netflix",
+    gallery: [{
+      src:"./assets/images/trend/7.png",
+      alt: "7"
+    },
+    {
+      src:"./assets/images/trend/8.png",
+      alt: "8"
+    },
+    {
+      src:"./assets/images/trend/9.png",
+      alt: "9"
+    },
+    {
+      src:"./assets/images/trend/10.png",
+      alt: "10"
+    },
+    {
+      src:"./assets/images/trend/11.png",
+      alt: "11"
+    },
+    {
+      src:"./assets/images/trend/12.png",
+      alt: "12"
     }]
    }
   
   
 
+   public trendingnow: MoviesInterface = {
+    title: "Trending Now",
+    gallery: [{
+      src:"./assets/images/trend/1.png",
+      alt: "1"
+    },
+    {
+      src:"./assets/images/trend/2.png",
+      alt: "2"
+    },
+    {
+      src:"./assets/images/trend/3.png",
+      alt: "3"
+    },
+    {
+      src:"./assets/images/trend/4.png",
+      alt: "4"
+    },
+    {
+      src:"./assets/images/trend/5.png",
+      alt: "5"
+    },
+    {
+      src:"./assets/images/trend/6.png",
+      alt: "6"
+    }]
+   }
 
 
   
